3fa81a88282b42aae877d1fade1cb4048363da19
Makkoas <makslololo135@gmail.com>
2025-07-11 16:09:57 +0300

    Betta-test

--- src/main/resources/templates/modal.js
+++ src/main/resources/templates/modal.js
@@ -1,15 +1,35 @@
-function openModal() {
-  document.getElementById("loginModal").style.display = "flex";
+
+function openModal(id) {
+  document.getElementById(id).style.display = "flex";
 }
 
-function closeModal() {
-  document.getElementById("loginModal").style.display = "none";
+
+function closeModal(id) {
+  document.getElementById(id).style.display = "none";
 }
+
+
+
+function switchModal(fromId, toId) {
+  closeModal(fromId);
+  openModal(toId);
+
+}
+
 
 window.onclick = function (event) {
-  const modal = document.getElementById("loginModal");
+  const loginModal = document.getElementById("loginModal");
+  const registerModal = document.getElementById("registerModal");
+  const resetPasswordModal = document.getElementById("resetPasswordModal");
 
-  if (event.target === modal) {
-    closeModal();
+  if (event.target === loginModal) {
+    closeModal("loginModal");
   }
+
+  if (event.target === registerModal) {
+    closeModal("registerModal");
+  }
+    if (event.target === resetPasswordModal) {
+        closeModal("resetPasswordModal");
+    }
 };
